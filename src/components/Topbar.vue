e0e52004c2f62ce4cdc2a58fe596dabdab29a197
ferwizawsky <ferryspedia14@gmail.com>
2024-01-18 09:58:46 +0700

    Add Notif Component

---
+++ src/components/Topbar.vue
@@ -0,0 +1,93 @@
+<script setup>
+import { useMyFetch, jsonFormData } from "@/composables/fetch.js";
+import { Menu, MenuButton, MenuItems, MenuItem } from "@headlessui/vue";
+import { ref, onMounted } from "vue";
+import { useAuth } from "@/stores/auth.js";
+import { useRouter } from "vue-router";
+
+const item = ref();
+const auth = useAuth();
+const router = useRouter();
+async function getUser() {
+  item.value = {};
+  try {
+    const { data } = await useMyFetch("GET", `/auth/profile`);
+    item.value = { ...data.data };
+    // location.reload();
+  } catch (error) {
+    logout();
+  } finally {
+  }
+}
+
+function logout() {
+  auth.token = "";
+  localStorage.removeItem("token");
+  router.push("/login");
+}
+onMounted(() => {
+  if (!item.value) {
+    getUser();
+  }
+});
+</script>
+<template>
+  <div
+    class="fixed top-0 z-20 bg-gray-100/10 backdrop-blur-sm left-0 w-full h-[60px] flex justify-between items-center px-4"
+  >
+    <div
+      class="font-medium tracking-widest flex items-center text-lg text-primary"
+    >
+      <svg
+        class="w-7 mr-2"
+        viewBox="0 0 46 57"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path d="M32.0547 0L0 32.0547H32.0547V0Z" fill="currentColor" />
+        <path
+          d="M45.5 34.246H0.167773L22.8339 56.9121L45.5 34.246Z"
+          fill="currentColor"
+        />
+      </svg>
+      <!-- <span>FIOEP</span> -->
+    </div>
+
+    <Menu as="div" class="relative inline-block text-left">
+      <div>
+        <MenuButton
+          class="w-8 h-8 cursor-pointer text-white bg-amber-300 flex items-center justify-center rounded-full"
+        >
+          {{ item?.name?.substring(0, 1) }}
+        </MenuButton>
+      </div>
+
+      <transition
+        enter-active-class="transition duration-100 ease-out"
+        enter-from-class="transform scale-95 opacity-0"
+        enter-to-class="transform scale-100 opacity-100"
+        leave-active-class="transition duration-75 ease-in"
+        leave-from-class="transform scale-100 opacity-100"
+        leave-to-class="transform scale-95 opacity-0"
+      >
+        <MenuItems
+          class="absolute right-0 mt-2 w-32 origin-top-right rounded-md bg-white shadow-lg focus:outline-none"
+        >
+          <div class="px-1 py-1">
+            <MenuItem v-slot="{ active }">
+              <button
+                @click="logout()"
+                :class="[
+                  active ? 'bg-primary text-white' : 'text-gray-900',
+                  'group flex w-full items-center rounded-md px-2 py-2 text-sm',
+                ]"
+              >
+                Logout
+              </button>
+            </MenuItem>
+          </div>
+        </MenuItems>
+      </transition>
+    </Menu>
+  </div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,18 +2,23 @@
 import { ref, onMounted, watch } from "vue";
 import { useNotif } from "@/stores/notif.js";
 import { useMyFetch, jsonFormData } from "@/composables/fetch.js";
-import Navbar from "@/components/Navbar.vue";
+import Topbar from "@/components/Topbar.vue";
 import Paginate from "@/components/UI/Paginate.vue";
 import { dateFormatter } from "@/composables/timeFormatter.js";
 
 const search = ref("");
 const page = ref(1);
+
 const list = ref([]);
+const listJadwal = ref([]);
+const listKelas = ref([]);
+
 const notif = useNotif();
 const meta = ref({
   links: [],
 });
 const wait = ref(false);
+
 async function getPost() {
   if (notif.loading) return;
   wait.value = false;
@@ -32,22 +37,34 @@ async function getPost() {
   }
 }
 
-async function getJadwal() {
+async function getKelas() {
+  if (notif.loading) return;
   wait.value = false;
-  notif.loading = true;
   list.value = [];
   try {
     const { data } = await useMyFetch(
       "GET",
-      `/student/jadwal?page=${page.value}&search=${search.value}&limit=8`
+      `/student/kelas?page=${page.value}&search=${search.value}&limit=80`
     );
-    console.log(data);
-    // list.value = [...data.absensi];
-    // meta.value = data.meta;
+    listKelas.value = [...data.data];
+    meta.value = data.meta;
   } catch (error) {
   } finally {
     notif.loading = false;
   }
+}
+
+async function getJadwal() {
+  wait.value = false;
+  listJadwal.value = [];
+  try {
+    const { data } = await useMyFetch(
+      "GET",
+      `/student/jadwal?page=${page.value}&search=${search.value}&limit=80`
+    );
+    listJadwal.value = [...data.absensi];
+    // meta.value = data.meta;
+  } catch (error) {}
 }
 
 watch(
@@ -85,60 +102,117 @@ function limitting(e) {
   return str;
 }
 onMounted(() => {
+  getKelas();
   getPost();
   getJadwal();
 });
 </script>
 <template>
-  <div
-    class="max-w-lg mx-auto pt-20 relative text-gray-600 min-h-screen pb-20 text-xs"
-  >
-    <Navbar />
+  <div class="max-w-lg mx-auto pt-20 relative min-h-screen pb-20 text-xs">
+    <Topbar />
 
     <div class="px-4">
-      <div class="pt-2 flex items-center justify-between">
-        <div class="text-xl">Hey there !</div>
-        <div>
-          <!-- <div
-            class="tracking-[7px] mb-2 font-semibold text-primary text-center"
-          >
-            POIN
+      <!-- <div class="">
+        <div class="grid grid-cols-3 gap-4 text-center text-white h-[90px]">
+          <div class="bg-rose-400 rounded-3xl p-3">
+            <div class="text-[10px]">Belum Cukup</div>
           </div>
+          <div class="bg-lime-600/60 rounded-3xl"></div>
+          <div class="bg-amber-400 rounded-3xl"></div>
+        </div>
+      </div> -->
+
+      <div>
+        <div>
           <div
-            class="w-[75px] h-[75px] flex items-center justify-center text-3xl font-semibold text-white rounded-full bg-primary"
+            class="bg-white mb-4 drop-shadow-md p-1.5 rounded-full flex items-center justify-start flex-wrap relative"
           >
-            50
-          </div> -->
+            <div class="p-4 bg-primary rounded-full text-white">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                class="w-6 h-6"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0"
+                />
+              </svg>
+            </div>
+            <div class="pl-3">
+              <div class="text-gray-400">UTS 4TI E Pemrograman Object</div>
+              <div class="text-lg font-semibold">16 Februari 2024</div>
+            </div>
+          </div>
         </div>
       </div>
 
-      <div class="pt-6">
+      <div class="pt-7 flex items-center justify-between">
+        <div class="text-lg font-semibold">Absensi</div>
+      </div>
+
+      <div class="pt-3">
         <div
-          v-for="index in list"
-          class="bg-white mb-4 p-4 rounded-xl border border-gray-200 flex items-center justify-between relative"
+          v-for="index in listKelas"
+          class="bg-white mb-4 drop-shadow-md p-4 rounded-2xl flex items-center justify-between flex-wrap relative"
         >
           <div class="pl-2">
-            <div class="font-semibold text-sm">
-              {{ index.kelas?.title }}
+            <div class="text-sm">
+              {{ index?.title }}
             </div>
-            <div class="text-xs">
-              <span class="text-gray-400 font-semibold">
-                {{ index.kelas?.author?.name }}</span
+            <div class="text-xs flex items-center pt-2 pb-1 text-gray-400">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="2"
+                stroke="currentColor"
+                class="w-4 h-4 mr-1"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+                />
+              </svg>
+
+              <span class="font-medium text-primary">
+                {{ index?.author?.name }}</span
               >
             </div>
-            <div class="pt-2 text-xs">
-              <span class="text-gray-400"> pada {{ index.kelas?.day }}</span>
+            <div class="text-xs flex items-center text-gray-400">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke-width="1.5"
+                stroke="currentColor"
+                class="w-4 h-4 mr-1"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+                />
+              </svg>
+              <span class=""> {{ index?.day }}</span>
             </div>
           </div>
           <div
             :class="
-              index.status == 'Belum Cukup'
-                ? 'bg-rose-400/20 text-rose-400'
-                : 'bg-primary/20 text-primary'
+              list.find((x) => x.kelas?.id == index.id)?.status == 'Cukup'
+                ? 'bg-primary/20 text-primary'
+                : 'bg-rose-400/20 text-rose-400'
             "
-            class="absolute top-2 right-2 py-1 text-xs px-3 flex items-center justify-center font-medium rounded-lg"
+            class="absolute bottom-2 right-2 py-1 text-xs px-3 flex items-center justify-center font-medium rounded-2xl"
           >
-            {{ index.status }}
+            {{
+              list.find((x) => x.kelas?.id == index.id)?.status ?? "Belum Cukup"
+            }}
           </div>
 
           <div
@@ -147,23 +221,9 @@ onMounted(() => {
                 ? ' text-gray-300'
                 : ' text-gray-700'
             "
-            class="absolute bottom-2 right-2 py-1 text-xs px-3 flex items-center justify-center font-medium rounded-lg"
+            class="absolute bottom-2 right-2 py-1 text-xs px-3 flex items-center justify-center font-medium rounded-2xl"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke-width="1.5"
-              stroke="currentColor"
-              class="w-4 h-4 mr-1"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-              />
-            </svg>
-            <div>{{ index.total }} / {{ index.min }}</div>
+            <!-- <div>{{ index.total }} / {{ index.min }}</div> -->
           </div>
         </div>
       </div>
