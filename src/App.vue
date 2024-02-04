4afd67989b1815815e12f29f4974bcacac8c29de
ferwizawsky <ferryspedia14@gmail.com>
2024-02-04 18:10:20 +0700

    Remove cached components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,15 @@
 <script setup>
 import { useOption } from "@/stores/option";
 import { defineAsyncComponent } from "vue";
+import Loading from "@/components/ui/Loading.vue";
+import NotifBar from "@/components/ui/NotifBar.vue";
 
-const Loading = defineAsyncComponent(() =>
-  import("@/components/ui/Loading.vue")
-);
-const NotifBar = defineAsyncComponent(() =>
-  import("@/components/ui/NotifBar.vue")
-);
+// const Loading = defineAsyncComponent(() =>
+//   import("@/components/ui/Loading.vue")
+// );
+// const NotifBar = defineAsyncComponent(() =>
+//   import("@/components/ui/NotifBar.vue")
+// );
 useOption().url = import.meta.env.VITE_API;
 useOption().name = import.meta.env.VITE_NAME;
 </script>
